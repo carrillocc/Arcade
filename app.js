4aecd5f051cb37be694b73131f210df5213c5b5b
Cindy Carrillo <carrillo201994@gmail.com>
2022-09-15 11:11:26 -0500

    added snake growth

--- app.js
+++ app.js
@@ -70,9 +70,15 @@ let snakeY;
 let velocityX = 0;
 let velocityY = 0;
 
+//snake body
+let snakeBody = [];
+
 //food
 let foodX;
 let foodY;
+
+//end of game
+let gameOver = false;
 
 
 window.onload = function () {
@@ -82,38 +88,80 @@ board.width = columns * blockSize;
 snakeRandomizer();
 foodRandomizer();
 document.addEventListener('keyup', changeDirection);
-update();
+// update();
+setInterval(update, 1000/10); //every 100 milliseconds runs update function
 }
 
 function update () {
+    if (gameOver) {
+        return;
+    }
+    
     //board rendering
     context.fillStyle = 'black';
     context.fillRect(0, 0, board.width, board.height);
 
-    //Snake rendering
-    context.fillStyle = 'lightblue';
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
     //food rendering
     context.fillStyle = 'red';
     context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    //colliding snake and food
+    if(snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]); //grows segment where food was
+        foodRandomizer(); //respawns food to random location function
+    }
+
+    //makes body follow head
+    for(let i = snakeBody.length -1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1]; //body gets previous x,y coordinates to move forward
+    }
+
+    //updates 2nd segment to take heads place (if there are body parts in the array)
+    if(snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    //Snake rendering
+    context.fillStyle = 'lightblue';
+    snakeX += velocityX * blockSize; //to move by unit not px
+    snakeY += velocityY * blockSize; //to move by unit not px
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    //drawing body
+    for(let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    //game over conditions
+    //out of grid
+    if (snakeX < 0 || snakeX > columns * blockSize || snakeY < 0 || snakeY > rows * blockSize) {
+        gameOver = true;
+        alert("Game Over");
+    }
+    //if snake overlaps
+    for (let i = 0; i < snakeBody.lendth; i++) {
+        if(snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
 }
 
-//changing directions (16:31)
+//changing directions --- != prevents snake from going back the direction it came from (16:31)
 function changeDirection(event) {
-    if(event.code == "ArrowUp") {
+    if(event.code == "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
     }
-    else if(event.code == "ArrowDown") {
+    else if(event.code == "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
     }
-    else if(event.code == "ArrowLeft") {
+    else if(event.code == "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
     }
-    else if(event.code == "ArrowRight") {
+    else if(event.code == "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
